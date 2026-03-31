3f8eaf612c3e3b9bfafaf22fe2cc1e3eaa8582f2
aaron decker <aaron@ard.ninja>
2026-03-31 11:00:50 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import sitemap from '@astrojs/sitemap'
 const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
 const isGithubPagesBuild =
   process.env.GITHUB_ACTIONS === 'true' || process.env.GITHUB_PAGES === 'true'
+const isUserOrOrgPagesRepo = repositoryName.endsWith('.github.io')
 
 const site = process.env.SITE_URL ?? 'https://leadtruffle.github.io'
 const base =
   process.env.BASE_PATH ??
-  (isGithubPagesBuild && repositoryName ? `/${repositoryName}` : '/')
+  (isGithubPagesBuild
+    ? isUserOrOrgPagesRepo || !repositoryName
+      ? '/'
+      : `/${repositoryName}`
+    : '/')
 
 export default defineConfig({
   site,
